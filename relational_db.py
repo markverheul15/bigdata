a0a9eae9774a5e478b3b53fe9b62b1f2a8011ffb
Lex <lexpoon96@gmail.com>
2021-03-29 21:56:01 +0200

    remove mongo_db.py

--- relational_db.py
+++ relational_db.py
@@ -1,9 +1,12 @@
 import mysql.connector
+import os
+import pandas as pd
 from mysql.connector import Error
+from sqlalchemy import create_engine
 
 
-# connect to MySQL server
-def create_connection(host_name, user_name, user_password):
+# Connect to MySQL server
+def create_connection_server(host_name, user_name, user_password):
     connection = None
     try:
         connection = mysql.connector.connect(
@@ -18,12 +21,7 @@ def create_connection(host_name, user_name, user_password):
     return connection
 
 
-# password = "ilemlbseicmzt2501527"
-password = "DFGZrzc9bm6Qg}P-"
-connection = create_connection("localhost", "root", password)
-
-
-# Create database RedditComments
+# Create databse in MySQL server
 def create_database(connection, query):
     cursor = connection.cursor()
     try:
@@ -33,12 +31,8 @@ def create_database(connection, query):
         print(f"The error '{e}' occurred")
 
 
-create_database_query = "CREATE DATABASE IMDB_movie_2020"
-create_database(connection, create_database_query)
-
-
-# Connect to MySQL database server after creation of database
-def create_connection(host_name, user_name, user_password, db_name):
+# Connect to MySQL database in server
+def create_connection_db(host_name, user_name, user_password, db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
@@ -54,9 +48,7 @@ def create_connection(host_name, user_name, user_password, db_name):
     return connection
 
 
-connection = create_connection("localhost", "root", password, "IMDB_movie_2020")
-
-
+# Execute queries in specific tables
 def execute_query(connection, query):
     cursor = connection.cursor()
     try:
@@ -66,14 +58,16 @@ def execute_query(connection, query):
     except Error as e:
         print(f"The error '{e}' occurred")
 
+
+# Define dataset tables
 create_name_basics_table = """
 CREATE TABLE IF NOT EXISTS name_basics (
   nconst VARCHAR(255) NOT NULL PRIMARY KEY,
   primaryName TEXT,
-  birthYear INT,
-  deathYear INT,
+  birthYear VARCHAR(255),
+  deathYear VARCHAR(255),
   primaryProfession TEXT,
-  knownForTitles TEXT
+  knownForTitles VARCHAR(255)
 ) ENGINE = InnoDB
 """
 
@@ -81,12 +75,12 @@ create_title_akas_table = """
 CREATE TABLE IF NOT EXISTS title_akas (
   titleId VARCHAR(255) NOT NULL PRIMARY KEY,
   ordering INT,
-  title TEXT,
+  title MEDIUMTEXT,
   region VARCHAR(255),
   language TEXT,
   types TEXT,
   attributes TEXT,
-  isOriginalTitle TINYINT(1)
+  isOriginalTitle VARCHAR(255)
 ) ENGINE = InnoDB
 """
 
@@ -96,10 +90,10 @@ CREATE TABLE IF NOT EXISTS title_basic (
   titleType VARCHAR(255),
   primaryTitle TEXT,
   originalTitle TEXT,
-  isAdult TINYINT(1),
-  startYear INT,
-  endYear INT,
-  runtimeMinutes INT,
+  isAdult INT,
+  startYear VARCHAR(255),
+  endYear VARCHAR(255),
+  runtimeMinutes VARCHAR(255),
   genres TEXT
 ) ENGINE = InnoDB
 """
@@ -107,8 +101,8 @@ CREATE TABLE IF NOT EXISTS title_basic (
 create_title_crew_table = """
 CREATE TABLE IF NOT EXISTS title_crew (
   tconst VARCHAR(255) NOT NULL PRIMARY KEY,
-  directors VARCHAR(255),
-  writers VARCHAR(255)
+  directors TEXT,
+  writers TEXT
 ) ENGINE = InnoDB
 """
 
@@ -120,10 +114,45 @@ CREATE TABLE IF NOT EXISTS title_rating (
 ) ENGINE = InnoDB
 """
 
+# Initialize MySQL server and connect to local database
+password = "DFGZrzc9bm6Qg}P-"
+# password = "ilemlbseicmzt2501527"
+connection = create_connection_server("localhost", "root", password)
+
+# Execute database creation
+create_database_query = "CREATE DATABASE IMDB_movie_2020"
+create_database(connection, create_database_query)
+
+# Connect to IMDB database
+connection = create_connection_db("localhost", "root", password, "IMDB_movie_2020")
+
+# Create tables in MySQl database
 execute_query(connection, create_name_basics_table)
 execute_query(connection, create_title_akas_table)
 execute_query(connection, create_title_basic_table)
 execute_query(connection, create_title_crew_table)
 execute_query(connection, create_title_rating_table)
 
-# LOAD DATA LOCAL INFILE 'D:/actors.list.tsv' INTO TABLE actors FIELDS TERMINATED BY '\t';
+# Create SQL
+engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
+                       .format(user="root", pw=password,
+                               db='IMDB_movie_2020'))
+dbConnection = engine.connect()
+directory = "IMDB_movie_2020/"
+
+# Insert datasets into database table via SQLalchemy engine
+for file in os.listdir(directory):
+    print("{} will now be inserted into the MySQL dataset".format(file))
+    table = file[:-4]
+    try:
+        chunk = pd.read_csv('IMDB_movie_2020/{}'.format(file), sep='\t', chunksize=1000000, low_memory=False)
+        df = pd.concat(chunk)
+        if file == "title_akas.tsv" or file == "name_basics.tsv":
+            df.drop_duplicates(subset=[df.columns[0]], keep='first', inplace=True)
+        df.to_sql(table, dbConnection, if_exists='append', chunksize=1000000, index=False)
+    except ValueError as vx:
+        print(vx)
+    except Exception as ex:
+        print(ex)
+    else:
+        print("{} dataset has been inserted".format(table))
