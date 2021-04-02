e3017a6f11b3fcd9f052f38131436192e4878321
Lex <lexpoon96@gmail.com>
2021-04-02 16:49:54 +0200

    mongo text

--- mongo_db.py
+++ mongo_db.py
@@ -8,9 +8,10 @@ def mongo_import(db_name, directory):
     # Connect to local Mongo_DB
     client = MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false")
 
+    f = open("times_mongo.txt", "w+")
+    f.writelines("Query1:\n")
     for file in os.listdir(directory):
         print("{} will now be inserted into the Mongo dataset".format(file))
-        start_time = time.time()
         table = file[:-4]
 
         chunk = pd.read_csv('IMDB_movie_2020/{}'.format(file), sep='\t', chunksize=1000000,
@@ -22,8 +23,15 @@ def mongo_import(db_name, directory):
         data_dict = data.to_dict("records")
 
         # Insert collection
+        start_time = time.time()
         collection.insert_many(data_dict)
+        start_time = time.time()
         print("{} dataset has been inserted".format(table))
         print("--- %s seconds ---" % (time.time() - start_time))
+        next_time = ("%s dataset took %s seconds ---\n" % (table, (time.time() - start_time)))
+        f.writelines(next_time)
+
+    return 0
+
 
 mongo_import("IMDB_movie_2020", "IMDB_movie_2020/")
